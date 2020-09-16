6aad89e19c92bbb6b9b4faa8aca0fbc324b42274
Rijan Chapagain <hrijon.chp@gmail.com>
2020-09-16 08:56:32 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace GeoPartial_RecommenderSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.map = new GMap.NET.WindowsForms.GMapControl();
+            this.textLat = new System.Windows.Forms.TextBox();
+            this.textLong = new System.Windows.Forms.TextBox();
+            this.latitude = new System.Windows.Forms.Label();
+            this.longitude = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(636, 573);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // map
+            // 
+            this.map.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.map.Bearing = 0F;
+            this.map.CanDragMap = true;
+            this.map.EmptyTileColor = System.Drawing.Color.Navy;
+            this.map.GrayScaleMode = false;
+            this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.map.LevelsKeepInMemory = 5;
+            this.map.Location = new System.Drawing.Point(0, 0);
+            this.map.MarkersEnabled = true;
+            this.map.MaxZoom = 2;
+            this.map.MinZoom = 2;
+            this.map.MouseWheelZoomEnabled = true;
+            this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.map.Name = "map";
+            this.map.NegativeMode = false;
+            this.map.PolygonsEnabled = true;
+            this.map.RetryLoadTile = 0;
+            this.map.RoutesEnabled = true;
+            this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.map.ShowTileGridLines = false;
+            this.map.Size = new System.Drawing.Size(636, 573);
+            this.map.TabIndex = 1;
+            this.map.Zoom = 0D;
+            // 
+            // textLat
+            // 
+            this.textLat.Location = new System.Drawing.Point(841, 125);
+            this.textLat.Name = "textLat";
+            this.textLat.Size = new System.Drawing.Size(116, 22);
+            this.textLat.TabIndex = 2;
+            // 
+            // textLong
+            // 
+            this.textLong.Location = new System.Drawing.Point(841, 211);
+            this.textLong.Name = "textLong";
+            this.textLong.Size = new System.Drawing.Size(116, 22);
+            this.textLong.TabIndex = 3;
+            // 
+            // latitude
+            // 
+            this.latitude.AutoSize = true;
+            this.latitude.Location = new System.Drawing.Point(746, 128);
+            this.latitude.Name = "latitude";
+            this.latitude.Size = new System.Drawing.Size(59, 17);
+            this.latitude.TabIndex = 4;
+            this.latitude.Text = "Latitude";
+            // 
+            // longitude
+            // 
+            this.longitude.AutoSize = true;
+            this.longitude.Location = new System.Drawing.Point(746, 214);
+            this.longitude.Name = "longitude";
+            this.longitude.Size = new System.Drawing.Size(71, 17);
+            this.longitude.TabIndex = 5;
+            this.longitude.Text = "Longitude";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(783, 293);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(147, 39);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 573);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.longitude);
+            this.Controls.Add(this.latitude);
+            this.Controls.Add(this.textLong);
+            this.Controls.Add(this.textLat);
+            this.Controls.Add(this.map);
+            this.Controls.Add(this.splitter1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Splitter splitter1;
+        private GMap.NET.WindowsForms.GMapControl map;
+        private System.Windows.Forms.TextBox textLat;
+        private System.Windows.Forms.TextBox textLong;
+        private System.Windows.Forms.Label latitude;
+        private System.Windows.Forms.Label longitude;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeoPartial_RecommenderSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            map.DragButton = MouseButtons.Left;
+            map.MapProvider = GMapProviders.GoogleMap;
+            double latitude = Convert.ToDouble(textLat.Text);
+            double longitude = Convert.ToDouble(textLong.Text);
+
+            map.Position = new PointLatLng(latitude, longitude);
+            map.MinZoom = 0;
+            map.MaxZoom = 18;
+            map.Zoom = 5;
+        }
+    }
+}
