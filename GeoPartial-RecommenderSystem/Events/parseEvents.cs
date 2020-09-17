6777bafcb8a46fc541312ff272fdefe957aa0710
Rijan Chapagain <hrijon.chp@gmail.com>
2020-09-17 17:03:45 +0800

    Adds LINQ to parse xml file

---
+++ GeoPartial-RecommenderSystem/Events/parseEvents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GeoPartial_RecommenderSystem.Events
+{
+    class parseEvents
+    {
+
+        public void LoadData()
+        {
+            // Load the XML file from our project directory containing the Events
+            var filename = "Lifelog-events.xml";
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var purchaseOrderFilepath = Path.Combine(currentDirectory, filename);
+
+            XElement purchaseOrder = XElement.Load(purchaseOrderFilepath);
+
+            IEnumerable<string> partNos = from item in purchaseOrder.Descendants("Item")
+                                          select (string)item.Attribute("PartNumber");
+            Console.WriteLine(partNos);
+        }
+       
+    }
+}
